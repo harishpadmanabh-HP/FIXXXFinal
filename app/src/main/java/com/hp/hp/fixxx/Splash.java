0508766184c68de2b83187803344b5effb118347
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-03-26 18:02:29 +0530

    fixr

--- app/src/main/java/com/hp/hp/fixxx/Splash.java
+++ app/src/main/java/com/hp/hp/fixxx/Splash.java
@@ -1,5 +1,6 @@
 package com.hp.hp.fixxx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,29 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Thread background = new Thread()
+        {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(3000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(Splash.this,Category_selection.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                    System.out.println(e);
+                }
+            }
+        };
+
+        // start thread
+        background.start();
     }
 }
